1219775efab0a607fd5e67f934c51c1198a33963
ThunderGer <Jaames11@hotmail.com>
2022-03-15 21:02:07 -0600

    /token ok

--- routes/dni.py
+++ routes/dni.py
@@ -1,7 +1,29 @@
 from fastapi import APIRouter
+import random
+import string
+import smtplib
 
 dni = APIRouter()
 
 @dni.get('/dates')
-def get_dates():
+def get_dates(DNI: str):
+    
     return "Lista la ruta :v"
+
+@dni.get('/token')
+def get_token(email: str):
+    str = string.ascii_lowercase+"1234567890"
+    token = ''.join(random.choice(str) for i in range(50))
+
+    credentials = {"email": "proculturamx98@gmail.com",
+                   "password": "2012010273.Well"}
+
+    conect = smtplib.SMTP(host='smtp.gmail.com', port=587)
+    conect.ehlo()
+
+    conect.starttls()
+    conect.login(user=credentials["email"], password=credentials["password"])
+    message = "Subject: Token for login in stdl\n"+token
+    conect.sendmail(from_addr=credentials["email"], to_addrs=email, msg=message)
+    conect.quit()
+    return {token}
